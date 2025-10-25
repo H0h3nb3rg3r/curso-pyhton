e6115dd7287f929961bad9c8e2636cebe2245cad
H0h3nb3rg3r <brunojosechapeco@gmail.com>
2025-10-24 23:41:39 -0300

    module 1

---
+++ module1/comments.py
@@ -0,0 +1,9 @@
+#one line comments
+#i can have a lot of comments
+#commments are green in visual code
+#ctrl c in an line will copy that and put in another line when pressed ctrl v
+#Comments are very useful in your code to help you or someone else understand:
+#- What your program does,
+#- What a particular line or section of code does;
+#- Why you chose to do something a particular way;
+#- Anything that might be helpful to know if you or someone else looking at the code later and trying to understand it.
