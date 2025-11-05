d6d1b803e1f1b50bbe04f1de186d7cb3f5d33510
H0h3nb3rg3r <brunojosechapeco@gmail.com>
2025-11-05 11:36:59 -0300

    diffent ways to print

---
+++ module 2/prints.py
@@ -0,0 +1,29 @@
+print('Hisotry Dickory Dock! he mouse ran up the clock')
+print("Hisotry Dickory Dock! he mouse ran up the clock")
+#can be used both to print - '' or ""
+
+print('The capybara is the worlds largest rodent')
+print('The capybara likes to live in groups')
+print("The capybara can swim")
+#it's better to use double quotes, in case you need to write for example: it's
+
+#there's another way to print in more than one line with just one print:
+print("History Dickory Dock! \nThe mouse ran up the clock!")
+#\n to insert a new line
+
+print("""line one
+line two
+      line three
+      line four...""")
+#you can use 3 quotes at the start and 3 quotes at the end in one print - this code makes you print in multiple lines with just one print
+
+print('here is a double quote"' + "here is a single quote'")
+print("or you can just do this \" does that work") 
+
+#\t gives a space in the line:
+print("what\thappens")
+
+print("can I just print a \ on the screen?\nYes, we can!")
+
+#I am inserting a \\ so the \ apperars correctly in the string:
+print("But what if I just want \\news")
