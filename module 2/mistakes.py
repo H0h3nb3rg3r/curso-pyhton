69b7ce4d1d845932e52e7c94392521b0a2499112
H0h3nb3rg3r <brunojosechapeco@gmail.com>
2025-11-07 11:11:15 -0300

    mistakes and solutions

---
+++ module 2/mistakes.py
@@ -0,0 +1,22 @@
+#print(History Dickory Dock)     #this is used for variables
+#print('It's a small world')     #use double quote when you're going to type it's
+#print("Hi there')               #you need to use only double quote ou just single quote in a print
+#prnit("Hello World!")           #you need to check if you type print in a right way
+
+
+#right way to type the codes:
+print("History Dickory Dock")
+print("It's a small world")
+print("Hi there")
+print("Hello World!")
+
+
+
+
+#challenge of the class:
+
+print('''There once was a movie star icon
+who preferred to sleep with the light on.
+They learned how to code
+a device that sure glowed
+and lit up the night using Python!''')
