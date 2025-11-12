03c0d80a91e5bbf90cbf37bba59175da446bb971
H0h3nb3rg3r <brunojosechapeco@gmail.com>
2025-11-12 11:02:25 -0300

    strings and variables

---
+++ module 3/strings_and_variables.py
@@ -0,0 +1,13 @@
+#Collect name from the user:
+name = input("What is your name?")
+#Display the name:
+print(name)
+
+
+#to the user type in a new line:
+name = input("What is your name?\n")
+print(name)
+
+#other way, but without the user typing - update the value:
+name = "Nicole"
+print(name)
