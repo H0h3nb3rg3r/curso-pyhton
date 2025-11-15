509eece9b5e52689002a22ccbc82ebaf6f07234b
H0h3nb3rg3r <brunojosechapeco@gmail.com>
2025-11-15 14:37:43 -0300

    numbers

---
+++ module 4/numbers.py
@@ -0,0 +1,39 @@
+age = 42 #don't use quotes to store numbers
+print(age)
+
+
+width = 29
+height = 5
+area = width * height
+print(area)
+perimeter = 2*width + 2*height
+#perimeter = 2*(width + height)    - both lines of perimeter are right
+print(perimeter)
+
+
+#Most common math operations:
+#Symbol | Operation      | Example
+# +     | addition       | 5+2=7
+# -     | subtraction    | 5-2=3
+# *     | multiplication | 5*2=10
+# /     | division       | 5/2=2.5
+# **    | exponent       | 5**2=25
+# %     | modulo         | 5%2=1   - modulo is the rest of a division
+#we don't use modulo so much on day by day, but we use it. We can use it to generate credit ou debit card numbers
+
+#Order of opperations:
+# () parentheses
+# ** exponent (e.g. **2 squared **3 cubed) - squarred: "ao quadrado", cubed: "ao cubo"
+# */ multiplication and division
+# +- addition and subtraction
+
+
+
+#e.g. program of the class:
+
+area = 0 #that line is optional
+height2 = 10
+width2 = 20
+#calculate the area of a triangle
+area = width2 * height2 /2
+print(area)
