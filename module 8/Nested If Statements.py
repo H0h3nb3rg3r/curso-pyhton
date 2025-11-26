3f37dfe9c7174b81b35c8d679f28c1c734130220
H0h3nb3rg3r <brunojosechapeco@gmail.com>
2025-11-25 22:57:08 -0300

    IF

---
+++ module 8/Nested If Statements.py
@@ -0,0 +1,17 @@
+monday = True
+freshCoffee = False
+if monday:
+    if not freshCoffee:
+        print("Go buy a coffee!") #If it's monday and you don't have fresh coffee, will print that
+    print("I have mondays")
+print("Now you can start work")
+
+
+#E.g.:
+team = input("Enter your favorite hockey team: ").upper()
+sport = input("Enter your favorite sport: ").upper()
+if sport == "HOCKEY":
+    print("Go hockey!!")
+    if team == "SENATORS":
+        print("Good luck getting the cup this year")
+        print("We do love hockey!!")
